4e9fccdd015966a6ce291521974a83783bc250d6
ggallo22 <92111625+ggallo22@users.noreply.github.com>
2023-01-29 20:31:02 -0800

    create inital nav bar

---
+++ comps/NavBar/NavBar.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function NavBar() {
+    return (
+        <>
+            <ul>
+                <li>
+                    <a href="/findrecipes">Find recipes</a>
+                </li>
+
+                <li>
+                    <a href="/feelinglucky">Feeling lucky?</a>
+                </li>
+
+                <li>
+                    <a href="/about">About us</a>
+                </li>
+            </ul>
+
+        </>
+    )
+}
