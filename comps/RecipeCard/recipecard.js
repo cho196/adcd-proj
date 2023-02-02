760744c23f4aaa4ef7180e74fca90c9b116c5152
ggallo22 <92111625+ggallo22@users.noreply.github.com>
2023-02-01 16:52:43 -0800

    update recipecard

---
+++ comps/RecipeCard/recipecard.js
@@ -0,0 +1,8 @@
+export default function RecipeCard({ h1text = "", src = "" }) {
+    return (
+        <div h1text="" src="" ptext="">
+            <img src={src} />
+            <h1>{h1text}</h1>
+        </div>
+    );
+}
