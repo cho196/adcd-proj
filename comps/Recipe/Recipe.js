65b3cc04e1869ff65db6c566d4342939e7bf97e4
cho196 <91933101+cho196@users.noreply.github.com>
2023-03-27 16:23:51 -0700

    display sugar content

--- comps/Recipe/Recipe.js
+++ comps/Recipe/Recipe.js
@@ -1,18 +1,33 @@
 import { useEffect } from "react";
+import styled from "styled-components";
+const Flex = styled.div`
+  display: flex;
+`;
+const Img = styled.img`
+  width: 600px;
+  height: 400px;
+  object-fit: cover;
+  border-radius: 0px 1rem 1rem 0px;
+`;
+
 export default function Recipe({
   h1text = "",
   src = "",
   ptext = "",
   ingredientData = "",
+  sugar = "",
 }) {
   const ingredientList = ingredientData[0];
-  console.log(ingredientList);
+  // console.log(ingredientList);
   return (
     <div h1text="" src="" ptext="">
-      <h1>{h1text}</h1>
-      <img src={src} />
+      <Flex>
+        <Img src={src} />
+        <h1>{h1text}</h1>
+        <h3>Sugar per recipe: {sugar}g</h3>
+      </Flex>
       <h3>Ingredients</h3>
-      {/* {ingredientList.map((ingredient, index) => {
+      {ingredientList?.map((ingredient, index) => {
         if (ingredient !== "undefined") {
           return (
             <div key={index}>
@@ -20,7 +35,7 @@ export default function Recipe({
             </div>
           );
         }
-      })} */}
+      })}
       <h3>Steps</h3>
       <p>{ptext}</p>
     </div>
