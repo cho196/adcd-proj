ba8bf21b5b754de48dbc162d8978ae9a1797ef38
cho196 <91933101+cho196@users.noreply.github.com>
2023-03-28 11:37:45 -0700

    add feeling lucky page

---
+++ pages/feelinglucky.js
@@ -0,0 +1,75 @@
+import { useEffect, useState } from "react";
+import Recipe from "../comps/Recipe/Recipe";
+import axios from "axios";
+import NavBar from "../comps/NavBar/NavBar";
+
+export default function FeelingLucky() {
+  const [recipe, setRecipe] = useState("");
+  const ingredientsObj = {};
+  const [ingredientsArray, setIngredientsArray] = useState([]);
+  const [sugar, setSugar] = useState("");
+  var sugarArray = [];
+
+  useEffect(() => {
+    axios
+      .get(`https://www.themealdb.com/api/json/v1/1/random.php`)
+      .then((response) => {
+        console.clear();
+        // console.log(response.data);
+        setRecipe(response.data.meals[0]);
+        // console.log(recipe);
+        for (let i = 0; i < 20; i++) {
+          if (
+            response.data.meals[0][`strIngredient${i}`] !== "" &&
+            response.data.meals[0][`strIngredient${i}`] !== undefined
+          ) {
+            ingredientsObj[response.data.meals[0][`strMeasure${i}`]] =
+              response.data.meals[0][`strIngredient${i}`];
+          }
+          setIngredientsArray([Object.entries(ingredientsObj)]);
+        }
+        let str = Object.keys(ingredientsObj)
+          .map((key) => `${key} ${ingredientsObj[key]}`)
+          .join(", ");
+        console.log(str);
+        axios
+          .get(`https://api.api-ninjas.com/v1/nutrition?query=${str}`, {
+            headers: {
+              "X-Api-Key": "PN+egjNuV73ERprjvn/T6Q==a3sRvM0NzFqd8Wr4",
+            },
+          })
+          .then((response) => {
+            for (let i = 0; i < response.data.length; i++) {
+              sugarArray.push(response.data[i].sugar_g);
+            }
+            console.log(sugarArray);
+            let result = sugarArray.reduce((a, b) => {
+              return a + b;
+            }, 0);
+
+            setSugar(result);
+          })
+          .catch((err) => {
+            console.log(err);
+            // setErrorMessage("Please enter a different ingredient");
+            // setNutritionResults();
+          });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return (
+    <>
+      <NavBar />
+      <Recipe
+        h1text={recipe.strMeal}
+        src={recipe.strMealThumb}
+        ptext={recipe.strInstructions}
+        ingredientData={ingredientsArray}
+        sugar={sugar}
+      ></Recipe>
+    </>
+  );
+}
